12ba8365722a1cd624de655d7caa896ba01c7df4
nikw46 <littleb890406@gmail.com>
2026-03-23 20:07:13 +0800

    Add files via upload

---
+++ blendData.js
@@ -0,0 +1,10 @@
+const blendData = [
+  {item:"香檳微盪",country:"衣索比亞 古吉、西達摩",roast:"淺焙",taste:"微酸",flavor:"香檳葡萄、荔枝、高山茶",priceHalf:"$200",drip:"$14"},
+  {item:"跳舞的羊",country:"西達摩、瓜地馬拉、耶加雪菲、肯亞",roast:"淺焙",taste:"微酸",flavor:"茂谷柑、小麥、蜜香",priceHalf:"$250",drip:"$19"},
+  {item:"果花飄逸",country:"西達摩、瓜地馬拉",roast:"中焙",taste:"微微酸",flavor:"香檳葡萄、玫瑰花、楓糖",priceHalf:"$250",drip:"$19"},
+  {item:"堅果森林",country:"薩爾瓦多、巴西、宏都拉斯、瓜地馬拉",roast:"中焙",taste:"不酸",flavor:"杏仁、煉乳、烏龍茶",priceHalf:"$250",drip:"$19"},
+  {item:"奶油果香",country:"西達摩、巴西、衣索比亞",roast:"中深焙",taste:"不酸",flavor:"黑櫻桃、黑巧克力、熟成柑橘、焦糖",priceHalf:"-",drip:"$19"},
+  {item:"奶油甜心",country:"巴西、哥倫比亞、宏都拉斯、西達摩、肯亞",roast:"中深焙",taste:"不酸",flavor:"榛果、巧克力牛奶、焦糖",priceHalf:"$250",drip:"-"},
+  {item:"麥田巧語",country:"巴西、哥倫比亞、泰國",roast:"中深焙",taste:"不酸",flavor:"麥芽、可可、榛果",priceHalf:"$200",drip:"$14"},
+  {item:"一釘一木",country:"巴西、尼加拉瓜、宏都拉斯",roast:"深焙",taste:"不酸",flavor:"堅果、黑巧克力、焦糖",priceHalf:"$250",drip:"$19"}
+];
